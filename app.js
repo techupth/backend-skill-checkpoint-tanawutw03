368d764f3c76e3bb04f40b8e2dd3c67d9263c3c7
Tanawut Wongboot <tanawut.w@outlook.com>
2024-01-16 12:56:11 +0700

    feat: connected mongodb and api for crud

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 import express from "express";
+import { client } from "./utils/db.js";
+import boardRouter from "./apps/boards.js";
 
 async function init() {
   const app = express();
   const port = 4000;
 
+  await client.connect();
+
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
+
+  app.use("/questions", boardRouter);
 
   app.get("/", (req, res) => {
     return res.json("Hello Skill Checkpoint #2");
@@ -14,6 +20,12 @@ async function init() {
   app.get("*", (req, res) => {
     return res.status(404).json("Not found");
   });
+
+  // app.get("/questions", (req, res) => {});
+  // app.get("/questions/:id", (req, res) => {});
+  // app.post("/questions", (req, res) => {});
+  // app.put("/questions/:id", (req, res) => {});
+  // app.delete("/questions/:id", (req, res) => {});
 
   app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
